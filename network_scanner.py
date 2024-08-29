b43221c9b48c22d7e59317f57cfbc6d54b713838
mckwk <147665058+mckwk@users.noreply.github.com>
2024-08-29 11:41:31 +0200

    minor improvements

--- network_scanner.py
+++ network_scanner.py
@@ -1,24 +1,60 @@
 import nmap
 from mac_vendor_lookup import MacLookup
+import socket
+import netifaces
 
 class NetworkScanner:
     def __init__(self, nmap_path):
         self.nmap_path = nmap_path
         self.mac_lookup = MacLookup()
 
-    def scan_network(self, target_ip):
-        nm = nmap.PortScanner(nmap_search_path=self.nmap_path)
-        # Use faster scan techniques and disable DNS resolution
-        nm.scan(hosts=target_ip, arguments='-O -T4 -n')
-        return [self._get_device_info(nm, host) for host in nm.all_hosts() if 'mac' in nm[host]['addresses']]
+    def scan_network(self):
+        try:
+            target_ip = self._discover_network_address()
+            nm = nmap.PortScanner(nmap_search_path=self.nmap_path)
+            nm.scan(hosts=target_ip, arguments='-O -T4 -n')
+            return [self._get_device_info(nm, host) for host in nm.all_hosts() if 'mac' in nm[host]['addresses']]
+        except Exception as e:
+            return []
+
+    def _discover_network_address(self):
+        try:
+            local_ip = socket.gethostbyname(socket.gethostname())
+            netmask = self._get_netmask(local_ip)
+
+            if netmask:
+                network_parts = [
+                    str(int(ip_part) & int(mask_part))
+                    for ip_part, mask_part in zip(local_ip.split("."), netmask.split("."))
+                ]
+                network_address = ".".join(network_parts) + "/24"
+                return network_address
+            else:
+                raise RuntimeError("Unable to determine network address")
+        except Exception as e:
+            raise RuntimeError("Unable to determine network address")
+
+    def _get_netmask(self, local_ip):
+        try:
+            for iface in netifaces.interfaces():
+                addrs = netifaces.ifaddresses(iface)
+                if netifaces.AF_INET in addrs:
+                    for addr in addrs[netifaces.AF_INET]:
+                        if addr["addr"] == local_ip:
+                            return addr.get("netmask")
+            return None
+        except Exception as e:
+            return None
 
     def _get_device_info(self, nm, host):
+        addresses = nm[host]['addresses']
+        mac = addresses['mac']
         return {
-            'ip': nm[host]['addresses']['ipv4'],
-            'mac': nm[host]['addresses']['mac'],
-            'vendor': self._lookup_vendor(nm[host]['addresses']['mac']),
-            'model': self._get_model(nm, host),
-            'product_id': self._get_product_id(nm, host),
+            'ip': addresses['ipv4'],
+            'mac': mac,
+            'vendor': self._lookup_vendor(mac),
+            'model': self._get_os_info(nm, host, 'osfamily', 'name', 'output'),
+            'product_id': self._get_os_info(nm, host, 'cpe', 'cpe', 'output'),
         }
 
     def _lookup_vendor(self, mac_address):
@@ -27,38 +63,14 @@ class NetworkScanner:
         except Exception:
             return "Unknown"
 
-    def _get_model(self, nm, host):
+    def _get_os_info(self, nm, host, *keys):
         try:
-            if 'osclass' in nm[host]:
-                for osclass in nm[host]['osclass']:
-                    if 'osfamily' in osclass:
-                        return osclass['osfamily']
-            if 'osmatch' in nm[host]:
-                for osmatch in nm[host]['osmatch']:
-                    if 'name' in osmatch:
-                        return osmatch['name']
-            if 'hostscript' in nm[host]:
-                for script in nm[host]['hostscript']:
-                    if 'output' in script:
-                        return script['output']
-            return "Unknown"
-        except Exception:
-            return "Unknown"
-
-    def _get_product_id(self, nm, host):
-        try:
-            if 'osclass' in nm[host]:
-                for osclass in nm[host]['osclass']:
-                    if 'cpe' in osclass:
-                        return osclass['cpe']
-            if 'osmatch' in nm[host]:
-                for osmatch in nm[host]['osmatch']:
-                    if 'cpe' in osmatch:
-                        return osmatch['cpe']
-            if 'hostscript' in nm[host]:
-                for script in nm[host]['hostscript']:
-                    if 'output' in script:
-                        return script['output']
+            for key in ['osclass', 'osmatch', 'hostscript']:
+                if key in nm[host]:
+                    for item in nm[host][key]:
+                        for subkey in keys:
+                            if subkey in item:
+                                return item[subkey]
             return "Unknown"
         except Exception:
             return "Unknown"
